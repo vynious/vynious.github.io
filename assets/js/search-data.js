f60e3f602993c15c1de207bbe5a01a86cb4fc59b
vynious <shawnthiah@gmail.com>
2025-09-18 08:06:11 +0000

    Deploying to gh-pages from @ vynious/vynious.github.io@80be2a1ae3e567229f74d462db8b2d715c780672 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,28 +51,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/al-folio/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/al-folio/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/al-folio/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/al-folio/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
