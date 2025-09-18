3dfc20480a64d55a4fea8ba5502aa4312de0bee9
vynious <shawnthiah@gmail.com>
2025-09-18 14:43:31 +0000

    Deploying to gh-pages from @ vynious/vynious.github.io@f4cac3630106aa935e9b6df855941f224038ee89 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -100,13 +100,6 @@ ninja.data = [{
           window.open("/feed.xml", "_blank");
         },
       },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
